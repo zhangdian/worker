32a26e076a40c703cbd999d0773a4941ba193e80
张典 <dianzhang@sohu-inc.com>
2012-12-29 11:13:51 +0800

    worker:v0.0.1.0

---
+++ src/main/java/com/bd17kaka/kankantu/weibo4j/Reminds.java
@@ -0,0 +1,27 @@
+package com.bd17kaka.kankantu.weibo4j;
+
+import com.bd17kaka.kankantu.weibo4j.model.PostParameter;
+import com.bd17kaka.kankantu.weibo4j.model.WeiboException;
+import com.bd17kaka.kankantu.weibo4j.org.json.JSONObject;
+import com.bd17kaka.kankantu.weibo4j.util.WeiboConfig;
+
+
+public class Reminds extends Weibo{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 获取某个用户的各种消息未读数 
+	 * 
+	 */
+	public JSONObject getUnreadCountOfRemind () throws WeiboException {
+		return client.get(WeiboConfig.getValue("rmURL") + "remind/unread_count.json").asJSONObject();
+	}
+	
+	public JSONObject getUnreadCountOfRemind (String callback) throws WeiboException {
+		return client.get(WeiboConfig.getValue("rmURL") + "remind/unread_count.json",new PostParameter[] {
+			new PostParameter("callback", callback)
+		}).asJSONObject();
+	}
+	
+}
--- src/main/java/com/bd17kaka/kankantu/worker/repost.java
+++ src/main/java/com/bd17kaka/kankantu/worker/repost.java
@@ -2,24 +2,51 @@ package com.bd17kaka.kankantu.worker;
 
 import redis.clients.jedis.Jedis;
 
+import com.bd17kaka.kankantu.weibo4j.Timeline;
+import com.bd17kaka.kankantu.weibo4j.model.WeiboException;
+
 public class repost {
 
 	private static String apiKey = "3790992882";
 	private static String apiSecret = "fa1b2986b8fcb881802c7ac811ef2d33";
 	private static String redisPassword = "199111@0Smy";
 	private static String redisHost = "69.85.92.97";
-	private static int redisPort = 6397;
-	
+	private static int redisPort = 6379;
 	/**
 	 * @param args
+	 * @throws WeiboException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException  {
 		// 获取redis对象
-//		Jedis jedis = new Jedis(redisHost, redisPort);
-//		jedis.auth(redisPassword);
+		Jedis jedis = new Jedis(redisHost, redisPort);
+		jedis.auth(redisPassword);
 		
-//		System.out.println(jedis.lpop("weibo:default:default"));
-		System.out.println("123");
+		while (true) {
+			// 等待5秒钟再转发
+			Thread.sleep(900000);
+			
+			// 获取token
+			String token = jedis.get("weibo:token");
+			if (null == token) {
+				continue;
+			}
+			
+			// 获取微博
+			String key = jedis.lpop("weibo:default:default");
+			if (key == null) {
+				continue;
+			}
+			Timeline timeLine = new Timeline();
+			timeLine.setToken(token);
+			// 转发微博
+			try {
+				timeLine.Repost(key);
+				System.out.println("转发微博成功:" + key);
+			} catch (WeiboException e) {
+				System.out.println(e.getMessage());
+				continue;
+			}
+		}
 	}
-
 }
